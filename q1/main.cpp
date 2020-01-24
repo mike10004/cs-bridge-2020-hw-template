5e91c6a6c8d2ce9230a66848d44d6a78e1c12772
Mike Chaberski <mchaberski@novetta.com>
2020-01-24 18:55:53 -0500

    q1: first implementation

--- q1/main.cpp
+++ q1/main.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+const double CLUB_CARD_DISCOUNT = 0.1;
+
 int main()
 {
-    std::string name;
-    std::cout << "Please enter your name: ";
-    std::cin >> name;
-    std::cout << "hello, " << name << std::endl;
+    string clubCardAnswer;
+    double item1Price, item2Price;
+    double minItemPrice, maxItemPrice;
+    double taxPercent, taxMultiplier;
+    double priceBase, priceAfterDiscount, priceTotal;
+
+    cout << "Enter price of first item: ";
+    cin >> item1Price;
+    cout << "Enter price of second item: ";
+    cin >> item2Price;
+    cout << "Does customer have a club card? (Y/N): ";
+    cin >> clubCardAnswer;
+    cout << "Enter tax rate, e.g. 5.5 for 5.5% tax: ";
+    cin >> taxPercent;
+
+    minItemPrice = item1Price;
+    maxItemPrice = item2Price;
+    if (item1Price > item2Price)
+    {
+        maxItemPrice = item1Price;
+        minItemPrice = item2Price;
+    }
+    priceBase = item1Price + item2Price;
+    priceAfterDiscount = maxItemPrice + (0.5 * minItemPrice);
+    if (clubCardAnswer == "y" || clubCardAnswer == "Y" || clubCardAnswer == "yes")
+    {
+        priceAfterDiscount = priceAfterDiscount * (1.0 - CLUB_CARD_DISCOUNT);
+    }
+    taxMultiplier = 1.0 + (taxPercent / 100.0);
+    priceTotal = priceAfterDiscount * taxMultiplier;
+
+    cout.setf(ios::fixed);
+    cout.precision(1);
+    cout << "Base price: " << priceBase << endl;
+    cout << "Price after discounts: " << priceAfterDiscount << endl;
+    cout.precision(5);
+    cout << "Total price: " << priceTotal << endl;
+
     return 0;
 }

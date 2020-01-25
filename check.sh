738b98c6f8dbf66dae83eade9967269c280896e2
Mike Chaberski <mchaberski@novetta.com>
2020-01-24 19:37:14 -0500

    check: fail with message when attempt to send input to process fails

--- check.sh
+++ check.sh
@@ -68,16 +68,27 @@ for Q in $QUESTIONS ; do
     sleep "$PAUSE"
     while IFS= read -r line
     do
+      debug "about to enter \"$line\""
+      set +e
       # Notice the line break in the line that follows
       screen -S "$SESSIONNAME" -X stuff "$line
 "
+      if [ $? -ne 0 ] ; then
+        echo "screen stuff failed; did the program terminate before all input was entered?"
+        exit 2
+      fi
+      set -e
       debug "sleeping ${PAUSE} after entering $line"
       sleep "$PAUSE"
     done < "$INFILE"
+    debug "quitting screen session $SESSIONNAME"
+    set -E
     screen -S "$SESSIONNAME" -X quit >/dev/null || true
+    set -e
     rm "$TEMPFILE" -f
     dos2unix -q -n screenlog.0 "$OUTFILE"
     rm screenlog.0
+    debug "checking output in $OUTFILE"
     if diff "$EXPECTEDFILE" "$OUTFILE" ; then
       RIGHTS="$Q $RIGHTS"
     else
